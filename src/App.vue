bcd46777886082bbc6e6c238055f1f2928eb29df
Tian_Mingfei <tianmingfei@taoshouyou.com>
2021-05-07 11:00:58 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="wrapper">
+    <div class="position">
+      <span class="iconfont position__icon">&#xe619;</span>
+        电子科技大学电子科技大学电子科技大学电子科技大学电子科技大学
+        <span class="iconfont position__notice">&#xe60b;</span>
+    </div>
+  </div>
+  <div class="docker">
+    <span class="docker__item docker__item--active">
+      <div class="iconfont">&#xe6f3;</div>
+      <div class="docker__title">首页</div>
+    </span>
+    <span class="docker__item">
+       <div class="iconfont">&#xe7e5;</div>
+      <div class="docker__title">购物车</div>
+    </span>
+    <span class="docker__item">
+      <div class="iconfont">&#xe61e;</div>
+      <div class="docker__title">订单</div>
+    </span>
+    <span class="docker__item">
+      <div class="iconfont">&#xe660;</div>
+      <div class="docker__title">我的</div>
+    </span>
+
+  </div>
+</template>
+
+<style lang="scss">
+@import './style/viriables.scss';
+@import './style/mixins.scss';
+.docker {
+  display: flex;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 0.49rem;
+  border-top: 1px solid #f1f1f1;
+  padding: 0 0.18rem;
+  box-sizing: border-box;
+  color: $content-fontcolor;
+
+  &__item {
+    flex: 1;
+    text-align: center;
+    .iconfont {
+      font-size: 0.2rem;
+      margin: 0.07rem 0.02rem 0;
+    }
+    &--active {
+      color: #1fa4fc;
+    }
+  }
+
+  &__title {
+    font-size: 20px;
+    transform: scale(0.5, 0.5);
+    transform-origin: center top;
+  }
+}
+
+.wrapper {
+  position: absolute;
+  left: 0;
+  top: 0;
+  bottom: 0.5rem;
+  right: 0;
+  padding: 0 0.18rem;
+}
+.position {
+  position: relative;
+  padding: 0.16rem 0.24rem 0.16rem 0;
+  line-height: 0.22rem;
+  font-size: 0.16rem;
+  @include ellipsis;
+  .position__icon {
+    position: relative;
+    top: 0.01rem;
+    font-size: 0.2rem;
+  }
+  .position__notice {
+    right: 0;
+    top: 0.17rem;
+    position: absolute;
+    font-size: 0.2rem;
+  }
+  color: $content-fontcolor;
+}
+
+</style>
