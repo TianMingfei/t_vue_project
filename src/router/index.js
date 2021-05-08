8b4595454bdca9b79b18db3bce4b8224972d8357
Tian_Mingfei <tianmingfei@taoshouyou.com>
2021-05-08 18:03:37 +0800

    添加router

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,17 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-// import Home from '../views/Home.vue'
-
+import Home from '../views/home/Home'
+import Login from '../views/login/Login'
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  }
   // {
   //   path: '/about',
   //   name: 'About',
@@ -20,6 +25,11 @@ const routes = [
 const router = createRouter({
   history: createWebHashHistory(),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  console.log(to, from)
+  next()
 })
 
 export default router
